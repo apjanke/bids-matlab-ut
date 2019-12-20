208837ff8382dae7e70ec8048b2f9c27d568dd6a
Andrew Janke <andrew@apjanke.net>
2019-12-20 06:10:54 -0500

    Fix coverage report to include subpackages

--- Mcode/+bidsut/run_all_tests.m
+++ Mcode/+bidsut/run_all_tests.m
@@ -11,35 +11,14 @@ import matlab.unittest.TestRunner
 import matlab.unittest.plugins.CodeCoveragePlugin
 import matlab.unittest.plugins.codecoverage.CoberturaFormat
 
-% This doesn't work because "Package folders are invalid inputs for
-% creating test suites" according to Matlab
-%tests_dir = fullfile(bidsut.Common.repoRootDir, 'Mcode', '+bidsut', '+tests');
-%suite = TestSuite.fromFolder(tests_dir)
+suite = TestSuite.fromPackage('bidsut.tests', 'IncludingSubpackages', true);
 
-% So we'll just list them all manually
-% suites = {
-%     TestSuite.fromClass(?bidsut.tests.ExamplesTest)
-%     TestSuite.fromClass(?bidsut.tests.QueryTest)
-%     TestSuite.fromClass(?bidsut.tests.MetadataTest)
-%     };
-% suite = [suites{:}];
-
-% Oh, wait, this seems to work:
-
-suite = TestSuite.fromPackage('bidsut.tests');
-
-% Matlab's CodeCoveragePlugin doesn't seem to recurse into subpackages,
-% so as a hack we'll produce two reports. Need to figure out how to merge
-% them.
 runner = TestRunner.withTextOutput;
 reportFile = 'coverage.xml';
 coveragePlugin = CodeCoveragePlugin.forPackage('bids', ...
-    'Producing',CoberturaFormat(reportFile));
+    'Producing',CoberturaFormat(reportFile), ...
+    'IncludingSubpackages', true);
 runner.addPlugin(coveragePlugin);
-reportFile2 = 'coverage2.xml';
-coveragePlugin2 = CodeCoveragePlugin.forPackage('bids.util', ...
-    'Producing',CoberturaFormat(reportFile2));
-runner.addPlugin(coveragePlugin2);
 
 out = runner.run(suite);
 
