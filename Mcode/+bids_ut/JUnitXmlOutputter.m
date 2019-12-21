19194002c667e9c901e578ad70c84c1c290c3c9d
Andrew Janke <andrew@apjanke.net>
2019-12-21 13:02:20 -0500

    Nicer JUnit XML output

--- Mcode/+bids_ut/JUnitXmlOutputter.m
+++ Mcode/+bids_ut/JUnitXmlOutputter.m
@@ -24,7 +24,7 @@ end
 % These are written as local functions just so I don't have to deal with so
 % much indentation.
 
-function writeXmlReport(file, tests, rslts)
+function writeXmlReport(file, allTests, allRslts)
 [fid,msg] = fopen(file, 'w');
 if fid < 1
     error('Failed opening file ''%s'' for writing: %s', file, msg);
@@ -32,33 +32,58 @@ end
 RAII.fid = onCleanup(@() fclose(fid));
 
     function p(fmt, varargin)
-        % p Just an abbreviation for fprintf()
-        fprintf(fid, [fmt '\n'], varargin{:});
+        % Just an abbreviation for fprintf()
+        fprintf(fid, fmt, varargin{:});
     end
 
-% TODO: Should each test class be presented as a different test suite?
-tbl = table(rslts);
-nTests = size(tbl, 1);
-nFailed = numel(find(tbl.Failed));
-duration = sum(tbl.Duration);
-p('<?xml version="1.0" encoding="UTF-8"?>');
-p('<testsuites>');
-p(['  <testsuite id="%d" name="BIDS-MATLAB Unit Tests" tests="%d" failures="%d" ' ...
-    'time="%f" hostname="%s">'], ...
-    0, nTests, nFailed, duration, bids_ut.Util.hostname);
-for i = 1:numel(tests)
-    test = tests(i);
-    rslt = rslts(i);
-    p('    <testcase name="%s" classname="%s" time="%f">', ...
-        test.Name, test.TestClass, rslt.Duration);
-    if rslt.Failed
-        p('      <failure/>');
-    elseif rslt.Incomplete
-        p('      <skipped/>');
+% Each test class is presented as a different test suite
+suites = testResultsToSuites(allTests, allRslts);
+p('<?xml version="1.0" encoding="UTF-8"?>\n');
+p('<testsuites>\n');
+for iSuite = 1:numel(suites)
+    suite = suites(iSuite);
+    tests = suite.tests;
+    rslts = suite.rslts;
+    tbl = table(suite.rslts);
+    nTests = size(tbl, 1);
+    nFailed = numel(find(tbl.Failed));
+    duration = sum(tbl.Duration);
+    p(['  <testsuite id="%d" name="%s" tests="%d" failures="%d" ' ...
+        'time="%f" hostname="%s">\n'], ...
+        iSuite-1, suite.testClass, nTests, nFailed, duration, bids_ut.Util.hostname);
+    for i = 1:numel(tests)
+        test = tests(i);
+        rslt = rslts(i);
+        fullTestName = test.Name;
+        shortTestName = fullTestName((numel(suite.testClass)+2):end);
+        p('    <testcase name="%s" classname="%s" time="%f">', ...
+            shortTestName, test.TestClass, rslt.Duration);
+        if rslt.Failed
+            p('<failure/>');
+        elseif rslt.Incomplete
+            p('<skipped/>');
+        end
+        p('</testcase>\n');
     end
-    p('    </testcase>');
+    p('  </testsuite>\n');
 end
-p('  </testsuite>');
-p('</testsuites>');
+p('</testsuites>\n');
+
+fprintf('Wrote tests results to %s\n', file);
 end
 
+
+function out = testResultsToSuites(tests, rslts)
+
+testClasses = [tests.TestClass];
+uTestClass = unique(testClasses);
+for i = 1:numel(uTestClass)
+    testClass = uTestClass{i};
+    tf = strcmp(testClasses, testClass);
+    suite.testClass = testClass;
+    suite.tests = tests(tf);
+    suite.rslts = rslts(tf);
+    out(i) = suite; %#ok<AGROW>
+end
+
+end
