f71b8d53787da05ab8aacab0649ad5df109a987e
Andrew Janke <andrew@apjanke.net>
2019-12-21 14:35:37 -0500

    Move all test output under test-output/

--- Mcode/+bids_ut/run_all_tests.m
+++ Mcode/+bids_ut/run_all_tests.m
@@ -3,6 +3,19 @@ function out = run_all_tests
 %
 % rslt = bids_ut.run_all_tests
 %
+% Runs all the tests in bids-matlab-ut, presenting results on the command
+% line and producing results output files.
+%
+% The results output files are
+% created in a directory named "test-output" under the current directory.
+% Output files:
+% test-output/
+%   junit/
+%     bids-matlab/
+%       results.xml     - JUnit XML format test results
+%   cobertura/
+%     coverage.xml      - Cobertura format code coverage report
+%
 % Examples:
 % bids_ut.run_all_tests
 
@@ -12,25 +25,33 @@ import matlab.unittest.plugins.CodeCoveragePlugin
 import matlab.unittest.plugins.codecoverage.CoberturaFormat
 import matlab.unittest.plugins.XMLPlugin;
 
+baseDir = pwd;
+testOutDir = [baseDir '/test-output'];
+if exist(testOutDir, 'dir')
+    rmdir(testOutDir, 's');
+end
+mkdir(testOutDir);
+
 suite = TestSuite.fromPackage('bids_ut.tests', 'IncludingSubpackages', true);
 
 runner = TestRunner.withTextOutput;
-reportFile = 'coverage.xml';
+mkdir([testOutDir '/cobertura']);
+coberturaOutFile = [testOutDir '/cobertura/coverage.xml'];
 coveragePlugin = CodeCoveragePlugin.forPackage('bids', ...
-    'Producing',CoberturaFormat(reportFile), ...
+    'Producing',CoberturaFormat(coberturaOutFile ), ...
     'IncludingSubpackages', true);
 runner.addPlugin(coveragePlugin);
 % This JUnit XML plugin is only in Matlab R2015b+
-% TODO: This should maybe be done wrt Common.repoRootDir instead of pwd
-mkdir('test-output/junit/bids-matlab');
+mkdir([testOutDir '/junit/bids-matlab']);
 junitXmlPlugin = XMLPlugin.producingJUnitFormat(...
-    'test-output/junit/bids-matlab/results.xml');
+    [testOutDir '/junit/bids-matlab/results.xml']);
 runner.addPlugin(junitXmlPlugin);
 
 out = runner.run(suite);
 
 % This is our DIY JUnit XML output
-%junitOutputter = bids_ut.JUnitXmlOutputter('test-output/junit/bids-matlab');
+% TODO: Replace this with a custom version of XMLPlugin
+%junitOutputter = bids_ut.JUnitXmlOutputter([testOutDir '/junit/bids-matlab']);
 %junitOutputter.writeReport(suite, out);
 
 end
